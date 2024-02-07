d7fd008e03303f30fda498bf8360ffe6011c36c0
Jude <JudePWhite@gmail.com>
2024-02-07 14:44:46 -0500

    Create Page9.java

---
+++ Page9.java
@@ -0,0 +1,13 @@
+/**
+ * Demo for Comments
+ * Jude White
+ * 2/7/2024
+ */
+
+public class Page9 {
+       public static void main(String[] args) {
+           System.out.println("Hi");
+           System.out.println("LabOneA");
+       }
+
+}
